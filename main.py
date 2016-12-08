1788b1e1e99a5ffe45a31fea6c3afbad92f4eb8e
Son Le <sonlexqt@gmail.com>
2016-12-09 04:57:52 +0700

    Restructure files and directories

--- main.py
+++ main.py
@@ -1,68 +1,295 @@
-# Import the cv2 library
 import cv2
+import numpy as np
+import math
 from utils import Utils
 
-IMAGE_1 = 'auld-lang-syne.jpg'
-IMAGE_2 = 'happy-birthday.jpg'
-IMAGE_3 = 'silent-night.jpg'
-IMAGE_4 = 'we-wish-you-a-merry-xmas.jpg'
-# Read the image you want connected components of
-images_path = 'images/scores/'
-music_score = IMAGE_1
-origin = cv2.imread(images_path + music_score)
-src = cv2.imread(images_path + music_score, cv2.CV_8UC1)
-# Threshold it so it becomes binary
-ret, thresh = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY_INV)
-# You need to choose 4 or 8 for connectivity type
-connectivity = 8
-# Perform the operation
-output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
-# Get the results
-# The first cell is the number of labels
-num_labels = output[0]
-print('num_labels:')
-print(num_labels)
-# The second cell is the label matrix
-labels = output[1]
-# The third cell is the stat matrix
-stats = output[2]
-# The fourth cell is the centroid matrix
-centroids = output[3]
+"""""""""""""""""""""""""""""""""""""""
+CONSTANTS
+"""""""""""""""""""""""""""""""""""""""
+WTITLE_IMG_SOURCE = "Source Image"
+WTITLE_IMG_ROI = "ROI Image"
+WTITLE_CANDIDATE_POINTS = "Candidate Points"
+WTITLE_IMG_ROTATED = "Rotated image"
+WTITLE_IMG_WITHOUT_STAFFLINES = "Image without staff lines"
+WTITLE_CONNECTED_COMPONENTS = "Connected components"
+MAX_ROTATION_ANGLE = 30
+MIN_ROTATION_ANGLE = - MAX_ROTATION_ANGLE
+REC_LINE_WIDTH = 2
+SPACE_BAR_KEY = 32
+IMG_FILE = 'images/scores/auld-lang-syne.jpg'
 
-rects_init = []
-rects_result = []
+"""""""""""""""""""""""""""""""""""""""
+GLOBAL VARIABLES
+"""""""""""""""""""""""""""""""""""""""
+is_dragging = False
+is_roi_selected = False
+is_roi_img_shown = False
+roi_ref_points = []
+# The matrices
+img = None
+img_roi = None
+img_candidate_points = None
+img_rotated = None
+img_without_staff_lines = None
 
-for i in range(0, num_labels):
-    left = stats[i, cv2.CC_STAT_LEFT]
-    top = stats[i, cv2.CC_STAT_TOP]
-    width = stats[i, cv2.CC_STAT_WIDTH]
-    height = stats[i, cv2.CC_STAT_HEIGHT]
-    right = left + width
-    bottom = top + height
-    # p1 = (left, top)
-    # p2 = (right, bottom)
-    # Need to convert the OpenCV coordinate system to Descartes coordinate system
-    reversed_p1 = (left, -top)
-    reversed_p2 = (right, -bottom)
-    rects_init.append([reversed_p1, reversed_p2])
+"""""""""""""""""""""""""""""""""""""""
+HELPER FUNCTIONS
+"""""""""""""""""""""""""""""""""""""""
 
-# Need to remove the biggest rectangle
-# TODO XIN this should be done in the "connectedComponentsWithStats" function above
-rects_init.pop(0)
-rects_result = Utils.remove_overlapping_rectangles(rects_init)
 
-for rect in rects_result:
-    left, top, right, bottom = Utils.get_rect_coordinates(rect)
-    # p1 = (left, top)
-    # p2 = (right, bottom)
-    # Now need to convert the Descartes coordinate system back to the OpenCV coordinate system
-    restored_p1 = (left, -top)
-    restored_p2 = (right, -bottom)
-    cv2.rectangle(origin, restored_p1, restored_p2, (0, 0, 255), 1, 8, 0)
+def mouse_drag_handler(event, x, y, flags, params):
+    global is_dragging, is_roi_selected, roi_ref_points, img, img_roi
 
-print(rects_init)
-print(len(rects_init))
-print(rects_result)
-print(len(rects_result))
-cv2.imshow("result", origin)
-cv2.waitKey(0)
+    if event == cv2.EVENT_LBUTTONDOWN and not is_dragging:
+        roi_ref_points = [(x, y)]
+        is_dragging = True
+
+    if event == cv2.EVENT_MOUSEMOVE and is_dragging:
+        img1 = img.copy()
+        if len(roi_ref_points) is 1:
+            roi_ref_points.append((x, y))
+        else:
+            roi_ref_points[1] = (x, y)
+        point1 = roi_ref_points[0]
+        point2 = roi_ref_points[1]
+        cv2.rectangle(img1, point1, point2, (0, 0, 255), REC_LINE_WIDTH, 8, 0)
+        cv2.imshow(WTITLE_IMG_SOURCE, img1)
+
+    if event == cv2.EVENT_LBUTTONUP and is_dragging:
+        if len(roi_ref_points) is 1:
+            roi_ref_points.append((x, y))
+        else:
+            roi_ref_points[1] = (x, y)
+        is_dragging = False
+        img_roi = img[roi_ref_points[0][1]:roi_ref_points[1][1], roi_ref_points[0][0]:roi_ref_points[1][0]]
+
+    if event == cv2.EVENT_LBUTTONUP:
+        is_dragging = False
+        is_roi_selected = True
+
+
+def is_this_pixel_removed(i, j, value, image):
+    height, width = image.shape[:2]
+    max_cols = width
+    if value > 0:
+        if i == 0 or i == 1:
+            return True
+        if image[i - 1, j] > 0:
+            if image[i - 2, j] > 0:
+                return False
+            if j >= 1 and image[i - 2, j - 1] > 0:
+                return False
+            if j < max_cols and image[i - 2, j + 1] > 0:
+                return False
+    return True
+
+
+def calculate_entropy(pthetas_avg, length):
+    res = 0
+    for i in range(0, length):
+        if pthetas_avg[i] != 0:
+            res += pthetas_avg[i] * math.log(pthetas_avg[i])
+    return -res
+
+"""""""""""""""""""""""""""""""""""""""
+STEPS
+"""""""""""""""""""""""""""""""""""""""
+
+
+def read_src_image():
+    # Step 1
+    global img
+    img = cv2.imread(IMG_FILE, 1)
+    if img is not None:
+        cv2.imshow(WTITLE_IMG_SOURCE, img)
+    else:
+        raise FileNotFoundError('Input image is not found')
+
+
+def roi_selection():
+    # Step 2
+    global img_roi, is_roi_img_shown
+    cv2.setMouseCallback(WTITLE_IMG_SOURCE, mouse_drag_handler)
+    while 1:
+        if is_roi_selected:
+            cv2.imshow(WTITLE_IMG_ROI, img_roi)
+            is_roi_img_shown = True
+        key = cv2.waitKey(0)
+        if key == SPACE_BAR_KEY and is_roi_img_shown:
+            break
+
+
+def candidate_points_extraction():
+    # Step 3
+    global img_roi, img_candidate_points
+    roi_img_gray = cv2.cvtColor(img_roi, cv2.COLOR_BGR2GRAY)
+    _, roi_img_thresh = cv2.threshold(roi_img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+    height, width = roi_img_thresh.shape[:2]
+    img_candidate_points = np.zeros((height, width), np.uint8)
+    for i in range(0, height):
+        for j in range(0, width):
+            pixel_gray_scale_value = roi_img_thresh[i, j]
+            if is_this_pixel_removed(i, j, pixel_gray_scale_value, roi_img_thresh):
+                img_candidate_points[i, j] = pixel_gray_scale_value
+    _, img_candidate_points = cv2.threshold(img_candidate_points, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow(WTITLE_CANDIDATE_POINTS, img_candidate_points)
+    cv2.waitKey(0)
+
+
+def rotation_angle_estimation():
+    # Step 4
+    global img, img_candidate_points, img_rotated
+    height, width = img_candidate_points.shape[:2]
+    entropy_ps_length = MAX_ROTATION_ANGLE - MIN_ROTATION_ANGLE + 1
+    entropy_ps = [0] * entropy_ps_length
+    for a in range(0, entropy_ps_length):
+        pthetas = [0] * height
+        pthetas_avg = [0] * height
+        sum_pthetas = 0
+        angle = a - MAX_ROTATION_ANGLE
+        center = (width / 2, height / 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated_roi_img = cv2.warpAffine(img_candidate_points, rotation_matrix, (height, width))
+        # The next calculation will use rotated_roi_img instead
+        for h in range(0, height):
+            sum_of_rows = 0
+            for w in range(0, width):
+                # TODO XIN weird error: index out of bounds (have to try-catch)
+                try:
+                    sum_of_rows += rotated_roi_img[h, w]
+                except IndexError:
+                    break  # Break out when list index out of range
+            pthetas[h] = sum_of_rows
+            sum_pthetas += pthetas[h]
+        for h in range(0, height):
+            pthetas_avg[h] = pthetas[h] / sum_pthetas
+        entropy_ps[a] = calculate_entropy(pthetas_avg, height)
+    min_a = 0
+    min_entropy = entropy_ps[min_a]
+    min_angle = min_a - MAX_ROTATION_ANGLE
+    for a in range(0, entropy_ps_length):
+        if entropy_ps[a] < min_entropy:
+            min_a = a
+            min_entropy = entropy_ps[a]
+            min_angle = min_a - MAX_ROTATION_ANGLE
+    print('Estimated rotation angle (deg):', min_angle)
+    img_height, img_width = img.shape[:2]
+    img_center = (img_height / 2, img_width / 2)
+    rotation_matrix = cv2.getRotationMatrix2D(img_center, min_angle, 1.0)
+    # Maintain white background when using OpenCV warpAffine
+    # (src, M, dsize, dst=None, flags=None, borderMode=None, borderValue=None)
+    # TODO XIN fix rotated_img offset problem
+    img_rotated = cv2.warpAffine(img, rotation_matrix, (img_width, img_height),
+                                 cv2.INTER_AREA, cv2.BORDER_DEFAULT, cv2.BORDER_REPLICATE)
+    cv2.imshow(WTITLE_IMG_ROTATED, img_rotated)
+    cv2.waitKey(0)
+
+
+def adaptive_removal():
+    # Step 5
+    global img_rotated, img_without_staff_lines
+    height, width = img_rotated.shape[:2]
+    pthetas = [0] * height
+    rotated_img_gray = cv2.cvtColor(img_rotated, cv2.COLOR_BGR2GRAY)
+    _, rotated_img_thresh = cv2.threshold(rotated_img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+    # Calculate horizontal projection
+    for h in range(0, height):
+        sum_of_rows = 0
+        for w in range(0, width):
+            sum_of_rows += rotated_img_thresh[h, w]
+        pthetas[h] = sum_of_rows
+    # Get the maximum of the projection(T)
+    T = pthetas[0]
+    for h in range(0, height):
+        if pthetas[h] > T:
+            T = pthetas[h]
+    # Then the projection is binarized using a threshold of half of the maximum
+    for h in range(0, height):
+        # TODO XIN currently using a hardcoded number 4 here
+        if pthetas[h] > (T / 4):
+            pthetas[h] = 255
+        else:
+            pthetas[h] = 0
+    # Estimate the staff line width
+    number_of_rows = 0
+    number_of_lines = 0
+    for h in range(0, height):
+        if pthetas[h] == 255:
+            number_of_rows += 1
+            if h >= 1 and pthetas[h - 1] != 255:
+                # Count this case as a new line
+                number_of_lines += 1
+    W = number_of_rows / number_of_lines
+    # Z is the number of times we need to run the staff line removal method
+    Z = math.ceil(W / 2)
+    img_without_staff_lines = rotated_img_thresh.copy()
+    wsl_height, wsl_width = img_without_staff_lines.shape[:2]
+    blank_rotated_img = np.zeros((wsl_height, wsl_width), np.uint8)
+    for z in range(0, Z):
+        for i in range(0, wsl_height):
+            for j in range(0, wsl_width):
+                pixel_gray_scale_value = img_without_staff_lines[i, j]
+                if is_this_pixel_removed(i, j, pixel_gray_scale_value, img_without_staff_lines):
+                    # Yes: copy this pixel to blank_rotated_img
+                    blank_rotated_img[i, j] = pixel_gray_scale_value
+        img_without_staff_lines = img_without_staff_lines - blank_rotated_img
+    # Perform dilation to restore the missing details
+    dilate_structuring_element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    img_without_staff_lines = cv2.dilate(img_without_staff_lines, dilate_structuring_element)
+    _, img_without_staff_lines = cv2.threshold(img_without_staff_lines, 127, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow(WTITLE_IMG_WITHOUT_STAFFLINES, img_without_staff_lines)
+    cv2.waitKey(0)
+    return 0
+
+
+def get_connected_components():
+    global img_without_staff_lines
+    img_without_staff_lines_rgb = cv2.cvtColor(img_without_staff_lines, cv2.COLOR_GRAY2RGB)
+    _, thresh = cv2.threshold(img_without_staff_lines, 127, 255, cv2.THRESH_BINARY_INV)
+    connectivity = 8
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+    rects_init = []
+    rects_result = []
+    for i in range(0, num_labels):
+        left = stats[i, cv2.CC_STAT_LEFT]
+        top = stats[i, cv2.CC_STAT_TOP]
+        width = stats[i, cv2.CC_STAT_WIDTH]
+        height = stats[i, cv2.CC_STAT_HEIGHT]
+        right = left + width
+        bottom = top + height
+        # p1 = (left, top)
+        # p2 = (right, bottom)
+        # Need to convert the OpenCV coordinate system to Descartes coordinate system
+        reversed_p1 = (left, -top)
+        reversed_p2 = (right, -bottom)
+        rects_init.append([reversed_p1, reversed_p2])
+    # TODO XIN need to remove the biggest rectangle
+    rects_init.pop(0)
+    rects_result = Utils.remove_overlapping_rectangles(rects_init)
+    for rect in rects_result:
+        left, top, right, bottom = Utils.get_rect_coordinates(rect)
+        # p1 = (left, top)
+        # p2 = (right, bottom)
+        # Now need to convert the Descartes coordinate system back to the OpenCV coordinate system
+        restored_p1 = (left, -top)
+        restored_p2 = (right, -bottom)
+        cv2.rectangle(img_without_staff_lines_rgb, restored_p1, restored_p2, (0, 0, 255), 1, 8, 0)
+    cv2.imshow(WTITLE_CONNECTED_COMPONENTS, img_without_staff_lines_rgb)
+    cv2.waitKey(0)
+    return 0
+
+"""""""""""""""""""""""""""""""""""""""
+MAIN
+"""""""""""""""""""""""""""""""""""""""
+
+
+def main():
+    read_src_image()
+    roi_selection()
+    candidate_points_extraction()
+    rotation_angle_estimation()
+    adaptive_removal()
+    get_connected_components()
+
+if __name__ == '__main__':
+    main()
--- tests/remove_overlapping_rectangles.py
+++ tests/remove_overlapping_rectangles.py
@@ -1,78 +1,68 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+# Import the cv2 library
+import cv2
 from utils import Utils
-from random import randint
 
+IMAGE_1 = 'auld-lang-syne.jpg'
+IMAGE_2 = 'happy-birthday.jpg'
+IMAGE_3 = 'silent-night.jpg'
+IMAGE_4 = 'we-wish-you-a-merry-xmas.jpg'
+# Read the image you want connected components of
+images_path = 'images/scores/'
+music_score = IMAGE_1
+origin = cv2.imread(images_path + music_score)
+src = cv2.imread(images_path + music_score, cv2.CV_8UC1)
+# Threshold it so it becomes binary
+ret, thresh = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY_INV)
+# You need to choose 4 or 8 for connectivity type
+connectivity = 8
+# Perform the operation
+output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+# Get the results
+# The first cell is the number of labels
+num_labels = output[0]
+print('num_labels:')
+print(num_labels)
+# The second cell is the label matrix
+labels = output[1]
+# The third cell is the stat matrix
+stats = output[2]
+# The fourth cell is the centroid matrix
+centroids = output[3]
 
-def generate_random_color():
-    r = lambda: randint(0, 255)
-    color = '#%02X%02X%02X' % (r(), r(), r())
-    return color
+rects_init = []
+rects_result = []
 
+for i in range(0, num_labels):
+    left = stats[i, cv2.CC_STAT_LEFT]
+    top = stats[i, cv2.CC_STAT_TOP]
+    width = stats[i, cv2.CC_STAT_WIDTH]
+    height = stats[i, cv2.CC_STAT_HEIGHT]
+    right = left + width
+    bottom = top + height
+    # p1 = (left, top)
+    # p2 = (right, bottom)
+    # Need to convert the OpenCV coordinate system to Descartes coordinate system
+    reversed_p1 = (left, -top)
+    reversed_p2 = (right, -bottom)
+    rects_init.append([reversed_p1, reversed_p2])
 
-def generate_random_rect(max_rect_size, max_plot_width, max_plot_height):
-    rect_width = randint(1, max_rect_size)
-    rect_height = randint(1, max_rect_size)
-    left = randint(-max_plot_width, max_plot_width - rect_width)
-    top = randint(-max_plot_height + rect_height, max_plot_height)
-    return [(left, top), (left + rect_width, top - rect_height)]
+# Need to remove the biggest rectangle
+# TODO XIN this should be done in the "connectedComponentsWithStats" function above
+rects_init.pop(0)
+rects_result = Utils.remove_overlapping_rectangles(rects_init)
 
-
-def generate_random_rects(max_rect_size, max_plot_width, max_plot_height, num_of_rects):
-    res = []
-    for i in range(0, num_of_rects):
-        rect = generate_random_rect(max_rect_size, max_plot_width, max_plot_height)
-        res.append(rect)
-    return res
-
-""" Constants """
-MAX_PLOT_WIDTH = 20
-MAX_PLOT_HEIGHT = 5
-MAX_RECT_SIZE = 4
-NUM_OF_RECTS = 30
-
-
-fig1 = plt.figure(figsize=(15, 15))
-ax1 = fig1.add_subplot(211, aspect='equal')
-ax1.set_xlim([-MAX_PLOT_WIDTH, MAX_PLOT_WIDTH])
-ax1.set_ylim([-MAX_PLOT_HEIGHT, MAX_PLOT_HEIGHT])
-ax2 = fig1.add_subplot(212, aspect='equal')
-ax2.set_xlim([-MAX_PLOT_WIDTH, MAX_PLOT_WIDTH])
-ax2.set_ylim([-MAX_PLOT_HEIGHT, MAX_PLOT_HEIGHT])
-
-rects_init = generate_random_rects(MAX_RECT_SIZE, MAX_PLOT_WIDTH, MAX_PLOT_HEIGHT, NUM_OF_RECTS)
-rects_final = Utils.remove_overlapping_rectangles(rects_init)
+for rect in rects_result:
+    left, top, right, bottom = Utils.get_rect_coordinates(rect)
+    # p1 = (left, top)
+    # p2 = (right, bottom)
+    # Now need to convert the Descartes coordinate system back to the OpenCV coordinate system
+    restored_p1 = (left, -top)
+    restored_p2 = (right, -bottom)
+    cv2.rectangle(origin, restored_p1, restored_p2, (0, 0, 255), 1, 8, 0)
 
 print(rects_init)
 print(len(rects_init))
-print(rects_final)
-print(len(rects_final))
-
-for rect in rects_init:
-    left, top, right, bottom = Utils.get_rect_coordinates(rect)
-    width = abs(right - left)
-    height = abs(top - bottom)
-    ax1.add_patch(
-        patches.Rectangle(
-            (left, bottom),  # (x,y) (left, bottom) or lower left
-            width,  # width
-            height,  # height
-            facecolor=generate_random_color(),
-            alpha=0.2,
-        )
-    )
-
-for rect in rects_final:
-    left, top, right, bottom = Utils.get_rect_coordinates(rect)
-    width = abs(right - left)
-    height = abs(top - bottom)
-    ax2.add_patch(
-        patches.Rectangle(
-            (left, bottom),  # (x,y) (left, bottom) or lower left
-            width,  # width
-            height,  # height
-            facecolor='red',
-            alpha=0.2,
-        )
-    )
-plt.show()
+print(rects_result)
+print(len(rects_result))
+cv2.imshow("result", origin)
+cv2.waitKey(0)
