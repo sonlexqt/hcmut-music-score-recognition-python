43ca2387b427f3317228b4c04b5f456fbdf8be92
Son Le <sonlexqt@gmail.com>
2016-12-14 15:41:54 +0700

    Refactor the symbols training data

--- symbols.py
+++ symbols.py
@@ -3,45 +3,45 @@ def get_symbol_by_index(idx):
     if idx is 0:
         symbol = 'DOT'
     elif idx is 1:
-        symbol = 'SHARP'
+        symbol = 'ACCIDENTAL_SHARP'
     elif idx is 2:
-        symbol = 'QUARTER_NOTE'
+        symbol = 'NOTE_QUARTER_UP'
     elif idx is 3:
-        symbol = 'HALF_NOTE'
+        symbol = 'NOTE_HALF_UP'
     elif idx is 4:
-        symbol = 'MEASURE_3_4'
+        symbol = 'TIME_SIGNATURE_3_4'
     elif idx is 5:
         symbol = 'BAR'
     elif idx is 6:
-        symbol = 'QUARTER_NOTE_REVERSED'
+        symbol = 'NOTE_QUARTER_DOWN'
     elif idx is 7:
-        symbol = 'BEAM_TWO_EIGHTH_NOTES'
+        symbol = 'BEAM_2_EIGHTH_NOTES_UP'
     elif idx is 8:
-        symbol = 'BEAM_TWO_EIGHTH_NOTES_REVERSED'
+        symbol = 'BEAM_2_EIGHTH_NOTES_DOWN'
     elif idx is 9:
         symbol = 'FINAL_BAR'
     elif idx is 10:
-        symbol = 'QUARTER_REST'
+        symbol = 'REST_QUARTER'
     elif idx is 11:
-        symbol = 'HALF_NOTE_REVERSED'
+        symbol = 'NOTE_HALF_DOWN'
     elif idx is 12:
-        symbol = 'EIGHTH_NOTE_REVERSED'
+        symbol = 'NOTE_EIGHTH_DOWN'
     elif idx is 13:
-        symbol = 'SHARP_DOUBLE'
+        symbol = 'KEY_SIGNATURE_DOUBLE_#'
     elif idx is 14:
-        symbol = 'TREBLE_CLEF'
+        symbol = 'CLEF_TREBLE'
     elif idx is 15:
-        symbol = 'EIGHT_NOTE'
+        symbol = 'NOTE_EIGHTH_UP'
     elif idx is 16:
-        symbol = 'HALF_NOTE_WITH_DOT'
+        symbol = 'NOTE_HALF_UP_WITH_DOT'
     elif idx is 17:
         symbol = 'TIE'
     elif idx is 18:
-        symbol = 'MEASURE-4-4'
+        symbol = 'TIME_SIGNATURE_4_4'
     elif idx is 19:
-        symbol = 'QUARTER_NOTE_WITH_DOT'
+        symbol = 'NOTE_QUARTER_UP_WITH_DOT'
     elif idx is 20:
-        symbol = 'WHOLE_NOTE'
+        symbol = 'NOTE_WHOLE'
     return symbol
 
 
