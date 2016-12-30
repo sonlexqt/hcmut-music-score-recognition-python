f3be3811db6d97a5824fe1fb85a71c938465b611
Son Le <sonlexqt@gmail.com>
2016-12-31 02:17:55 +0700

    Add adaptive thresholding

---
+++ tests/thresholding/otsu.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+IMG_FILE = '../../images/scores/scan/jingle-bells.jpg'
+img = cv2.imread(IMG_FILE)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# global thresholding
+ret1, th1 = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+# Otsu's thresholding
+ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+blur = cv2.GaussianBlur(img, (5, 5), 0)
+ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1, img, 0, th2, blur, 0, th3]
+titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v=127)',
+          'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+          'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
+for i in range(3):
+    plt.subplot(3, 3, i*3+1), plt.imshow(images[i*3], 'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i*3+2), plt.hist(images[i*3].ravel(), 256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i*3+3), plt.imshow(images[i*3+2], 'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# Xin's modifications
+filter = cv2.medianBlur(img, 3)
+ret4, th4 = cv2.threshold(filter, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+cv2.imshow('filtered', filter)
+cv2.imshow('th4', th4)
+cv2.waitKey()
